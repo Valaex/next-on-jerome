10830d520aa776c9bc9c8421b19714bca0557e30
Jérôme Knops <knopsjeromepro@gmail.com>
2023-05-11 13:40:51 +0200

    button changes

--- src/components/Button/ContactButton.tsx
+++ src/components/Button/ContactButton.tsx
@@ -9,11 +9,13 @@ type Props = {};
 
 function ContactButton({}: Props) {
   return (
-    <div
-      className={`${montserrat.className} cursor-pointer hover:bg-slate-200 hover:text-[#040320] hover:transition-all hover:duration-300 duration-300 hover:shadow-xl hover:shadow-[#040320] flex flex-row justify-center items-center p-4 gap-2 w-48 h-12 left-24  bg-[#040320]  rounded-full text-white font-bold text-xs sm:text-xs  tracking-widest`}
-    >
-      <a href="#contact">ME CONTACTER</a>
-    </div>
+    <a href="#contact">
+      <div
+        className={`${montserrat.className} cursor-pointer hover:bg-slate-200 hover:text-[#040320] hover:transition-all hover:duration-300 duration-300 hover:shadow-xl hover:shadow-[#040320] flex flex-row justify-center items-center p-4 gap-2 w-48 h-12 left-24  bg-[#040320]  rounded-full text-white font-bold text-xs sm:text-xs  tracking-widest`}
+      >
+        ME CONTACTER
+      </div>
+    </a>
   );
 }
 
--- src/components/Button/DevisButton.tsx
+++ src/components/Button/DevisButton.tsx
@@ -9,11 +9,13 @@ type Props = {};
 
 function DevisButton({}: Props) {
   return (
-    <div
-      className={`${montserrat.className} cursor-pointer hover:bg-slate-200 hover:text-[#040320] hover:transition-all hover:duration-300 duration-300 hover:shadow-xl hover:shadow-[#040320] text-center p-4 gap-2 md:w-[180px]  lg:w-[264px]  left-24  bg-[#040320] rounded-2xl text-white font-bold md:text-xs lg:text-[18px]  tracking-widest`}
-    >
-      <a href="#contact">Demandez un Devis</a>
-    </div>
+    <a href="#contact">
+      <div
+        className={`${montserrat.className} cursor-pointer hover:bg-slate-200 hover:text-[#040320] hover:transition-all hover:duration-300 duration-300 hover:shadow-xl hover:shadow-[#040320] text-center p-4 gap-2 md:w-[180px]  lg:w-[264px]  left-24  bg-[#040320] rounded-2xl text-white font-bold md:text-xs lg:text-[18px]  tracking-widest`}
+      >
+        Demandez un Devis
+      </div>
+    </a>
   );
 }
 
