9146cbe2c18983ad6afa58701941177e362afd7e
Jérôme Knops <109312090+Valaex@users.noreply.github.com>
2023-07-09 18:00:27 +0200

    Update ContactButton.tsx

--- src/components/Button/ContactButton.tsx
+++ src/components/Button/ContactButton.tsx
@@ -11,7 +11,7 @@ function ContactButton({}: Props) {
   return (
     <a href="#contact">
       <div
-        className={`${montserrat.className} cursor-pointer hover:bg-white hover:text-[#040320] hover:transition-all hover:duration-300 duration-300 hover:shadow-xl hover:shadow-[#040320] flex flex-row justify-center items-center p-4 gap-2 w-48 h-12 left-24  bg-[#040320]  rounded-full text-white font-bold text-[9px] md:text-xs  tracking-widest`}
+        className={`${montserrat.className} cursor-pointer hover:bg-white hover:text-[#040320] hover:transition-all hover:duration-300 duration-300 hover:shadow-xl hover:shadow-[#040320] flex flex-row justify-center items-center p-2 md:p-4 gap-2 w-36 md:w-42 h-10 md:h-12 left-24  bg-[#040320]  rounded-full text-white font-bold text-[9px] md:text-xs  tracking-widest`}
       >
         DEVIS GRATUIT
       </div>
